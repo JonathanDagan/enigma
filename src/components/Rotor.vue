0dee283c76fda52e766a1abb52405a4c1a84c060
Jonathan Dagan <jonathandagan12@gmail.com>
2022-12-25 02:00:58 +0200

    rottor component

--- src/components/Rotor.vue
+++ src/components/Rotor.vue
@@ -1,21 +1,93 @@
 <script lang="ts">
-export default {
 
+interface rottorMapping {
+    [key: string]: string;
 }
+
+const defaultRotorMapping: rottorMapping = {
+    'a': 'b',
+    'b': 'c',
+    'c': 'd',
+    'd': 'e',
+    'e': 'f',
+    'f': 'g',
+    'g': 'h',
+    'h': 'i',
+    'i': 'j',
+    'j': 'k',
+    'k': 'l',
+    'l': 'm',
+    'm': 'n',
+    'n': 'o',
+    'o': 'p',
+    'p': 'q',
+    'q': 'r',
+    'r': 's',
+    's': 't',
+    't': 'u',
+    'u': 'v',
+    'v': 'w',
+    'w': 'x',
+    'x': 'y',
+    'y': 'z',
+    'z': 'a'
+}
+export default {
+    props: {
+        notch: {
+            type: String,
+            required: false,
+            default: 'a',
+        },
+        mapping: {
+            type: Object as () => rottorMapping,
+            required: false,
+            default: defaultRotorMapping,
+        },
+    },
+    data() {
+        return {
+            notch: this.notch,
+            mapping: this.mapping,
+        };
+    },
+    methods: {
+        stepThroughAlphabet(direction: 'forward' | 'backward' = 'forward'): string {
+            const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+            let currentIndex = alphabet.indexOf(this.notch);
+            if (direction === 'forward') {
+                currentIndex += 1;
+                if (currentIndex > 25) {
+                    currentIndex = 0;
+                }
+            } else {
+                currentIndex -= 1;
+                if (currentIndex < 0) {
+                    currentIndex = 25;
+                }
+            }
+            return alphabet[currentIndex];
+        },
+        shift() {
+            return this.mapping[this.notch];
+        }
+    }
+}
+
 </script>
 
 <template>
     <div class="rotor">
-        <div>aaa</div>
-        <div>bbb</div>
+        <button @click="stepThroughAlphabet('backward')">+</button>
+        <div>{{ notch }}</div>
+        <button @click="stepThroughAlphabet('backward')">-</button>
     </div>
 </template>
 
 <style>
 .rotor {
     display: flex;
-    flex-direction: row;
-
+    flex-direction: column;
     border: 5px solid;
     border-color: blue;
 }
