f9cef536a442f1b2ab4fbd410c348bb54a466172
Jonathan <jonathandagan12@gmail.com>
2022-12-27 01:51:22 +0200

    added emiting to plugboard

--- src/components/Plugboard.vue
+++ src/components/Plugboard.vue
@@ -1,92 +1,107 @@
 <template>
-    <form class="plugboard" @submit.prevent="plug"></form>
-    <span class="plug-row" v-for="row in rows">
-        <label class="plug" v-for="key in row" for="key">{{ key.toUpperCase() }}
-            <input type="checkbox" v-model="selectedPlugs"
-                :disabled="(selectedPlugs.length >= 2 && !selectedPlugs.includes(key)) || isPlugged(key)"
-                :value="key" />
-        </label>
-    </span>
-    <button type="submit" :disabled="selectedPlugs.length !== 2" @click="plug">Plug</button>
-    <button type="submit" :disabled="!plugedPairs.length" @click="unplug">Unplug</button>
+  <form class="plugboard" @submit.prevent="plug"></form>
+  <span class="plug-row" v-for="row in rows">
+    <label class="plug" v-for="key in row" for="key"
+      >{{ key.toUpperCase() }}
+      <input
+        type="checkbox"
+        v-model="selectedPlugs"
+        :disabled="
+          (selectedPlugs.length >= 2 && !selectedPlugs.includes(key)) ||
+          isPlugged(key)
+        "
+        :value="key"
+      />
+    </label>
+  </span>
+  <button type="submit" :disabled="selectedPlugs.length !== 2" @click="plug">
+    Plug
+  </button>
+  <button type="submit" :disabled="!plugedPairs.length" @click="unplug">
+    Unplug
+  </button>
 </template>
-  
-<script lang="ts">
-import { defineComponent } from 'vue'
 
-const BASIC_COLORS = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+<script lang="ts">
+import { defineComponent } from "vue";
+
 
 export default defineComponent({
-    name: 'Plugboard',
-    data() {
-        return {
-            rows: [
-                ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p'],
-                ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l'],
-                ['z', 'x', 'c', 'v', 'b', 'n', 'm'],
-            ],
-            selectedPlugs: [] as string[],
-            plugedPairs: [] as string[][],
-        };
+  name: "Plugboard",
+  data() {
+    return {
+      rows: [
+        ["q", "w", "e", "r", "t", "y", "u", "i", "o", "p"],
+        ["a", "s", "d", "f", "g", "h", "j", "k", "l"],
+        ["z", "x", "c", "v", "b", "n", "m"],
+      ],
+      selectedPlugs: [] as string[],
+      plugedPairs: [] as string[][],
+    };
+  },
+  computed: {
+    selectedPlugValues() {
+      return this.selectedPlugs.slice(0, 2);
     },
-    computed: {
-        selectedPlugValues() {
-            return this.selectedPlugs.slice(0, 2);
-        }
+  },
+  methods: {
+    isPlugged(letter: string): boolean {
+      return this.plugedPairs.some((pair) => {
+        return pair.includes(letter);
+      });
     },
-    methods: {
-        isPlugged(letter: string): boolean {
-            return this.plugedPairs.some((pair) => {
-                return pair.includes(letter);
-            })
-        },
-        plug() {
-            this.plugedPairs.push([...this.selectedPlugValues]);
-            this.selectedPlugs = [];
-        },
-        unplug() {
-            this.plugedPairs = [];
-        },
+    plug() {
+      this.plugedPairs.push([...this.selectedPlugValues]);
+      this.selectedPlugs = [];
     },
+    unplug() {
+      this.plugedPairs = [];
+    },
+    passThroughPlugboard(letter: string): void {
+      const pairs = this.plugedPairs;
+      const pair = pairs.find(
+        ([letter1, letter2]) => letter1 === letter || letter2 === letter
+      );
+      this.$emit("plugboard", pair ? (pair[0] === letter ? pair[1] : pair[0]) : letter);
+    },
+  },
 });
-
 </script>
-  
+
 <style>
 .plugboard {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
 }
 
 .input {
-    border-radius: 50%;
-    width: 30px;
-    height: 30px;
-    border: 1px solid black;
-    background-color: #eee;
-    margin: 2px;
+  border-radius: 50%;
+  width: 30px;
+  height: 30px;
+  border: 1px solid black;
+  background-color: #eee;
+  margin: 2px;
 }
 
 .plugboard input.pressed {
-    background-color: rgb(184, 175, 44);
+  background-color: rgb(184, 175, 44);
 }
 
 .label {
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    flex-direction: column;
-    padding: auto;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  padding: auto;
 }
 
 .plug-row {
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 </style>
-  
